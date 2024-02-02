19ec6327d26abde550f1497468cc668a4df1adad
sashadmii <145040161+sashadmii@users.noreply.github.com>
2024-02-02 17:50:47 +0100

    project.js

---
+++ project.js
@@ -0,0 +1,131 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+const valid6 = [4, 5, 3, 9, 6, 8, 9, 8, 8, 7, 7, 0, 5, 7, 9, 8];
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+// An array of all the arrays above
+const batch = [
+  valid1,
+  valid2,
+  valid3,
+  valid4,
+  valid5,
+  invalid1,
+  invalid2,
+  invalid3,
+  invalid4,
+  invalid5,
+  mystery1,
+  mystery2,
+  mystery3,
+  mystery4,
+  mystery5,
+];
+
+// Add your functions below:
+const validateCred = (arr) => {
+  // reverse array
+  const reversed = arr.reverse();
+
+  // double the numbers in array (except the 1st) + extract 9 if the doubled number is bigger then 9
+  let doubled = [];
+
+  for (let i = 0; i < reversed.length; i++) {
+    const num = () => {
+      let doublednum = reversed[i] * 2;
+      if (doublednum > 9) {
+        return doublednum - 9;
+      } else {
+        return doublednum;
+      }
+    };
+
+    if (i === 0) {
+      doubled.push(reversed[i]);
+    } else if (i % 2 != 0) {
+      doubled.push(num());
+    } else {
+      doubled.push(reversed[i]);
+    }
+  }
+
+  // sum up the digits
+  let sum = 0;
+
+  for (let i = 0; i < doubled.length; i++) {
+    sum += doubled[i];
+  }
+
+  // checkig the %
+  if (sum % 10 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const findInvalidCards = (arr) => {
+  let invalidCards = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (validateCred(arr[i]) === false) {
+      invalidCards.push(arr[i].reverse());
+    }
+  }
+  return invalidCards;
+};
+
+let cardsArray = findInvalidCards(batch);
+
+const idInvalidCardCompanies = (arr) => {
+  let cardsCompanies = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === 3) {
+      cardsCompanies.push("Amex (American Express)");
+    } else if (arr[i][0] === 4) {
+      cardsCompanies.push("Visa");
+    } else if (arr[i][0] === 5) {
+      cardsCompanies.push("Mastercard");
+    } else if (arr[i][0] === 6) {
+      cardsCompanies.push("Discover");
+    } else {
+      cardsCompanies.push("Company not found");
+    }
+  }
+  let unique = [];
+  cardsCompanies.forEach((company) => {
+    if (!unique.includes(company)) {
+      unique.push(company);
+    }
+  });
+
+  console.log(unique);
+};
+
+idInvalidCardCompanies(cardsArray);
+
+
+// a function that accepts a string and converts it into an array of numbers like the initially provided arrays
+
+const string = '1128265363'
+const strToArr = (str) => str.split('');
+console.log(strToArr(string));
+
+
